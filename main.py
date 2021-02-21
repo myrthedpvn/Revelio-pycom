5a862bb43890024a830c3d1c06b5e9406015083a
myrthedpvn <74008279+myrthedpvn@users.noreply.github.com>
2021-02-21 14:35:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from machine import UART
+
+uart = UART(1)
+uart.init(9600, bits=8, parity=None, stop=1, timeout_chars=100, pins=('P3', 'P4'))
+
+while True:
+    header_bytes = uart.read(1)
+    while(header_bytes != b'\xff'):
+        header_bytes=uart.read(1)
+    data_high = int(uart.read(1)[0])
+    data_low = int(uart.read(1)[0])
+    data_sum = int(uart.read(1)[0])
+    sum = data_high + data_low
+    if (sum == data_sum + 1):
+        distance = (data_high*256 + data_low)/10
+        format = "{:6.3f}".format(distance)
+        print("de afstand bedraagt" + format + " cm")
