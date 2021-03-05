26ec088faf0d2ccdcc6e1b1b861dc6e244151faf
myrthedpvn <74008279+myrthedpvn@users.noreply.github.com>
2021-03-05 11:50:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import wifi
+import sonar
+import lora
+import time
+import wifiadafruit
 
+lora.connect()
+sonar.read_uart()
+
+#Zet degene die je niet gebruikt in comments
+while True:
+    sonar.read_uart()
+    distance = sonar.get_distance()
+    lora.send(distance)
+    print(distance)
+    time.sleep(10)
+
+#wifi.connect()
+#sonar.read_uart()
+
+#while True:
+    #sonar.read_uart()
+    #distance = sonar.get_distance()
+    #wifiadafruit.sendDataWifi(distance)
+    #print(distance)
+    #time.sleep(10)
